a6975ffd0a751859f4c708948b21e635cb31fa54
vMaziii <145691664+vMaziii@users.noreply.github.com>
2026-04-15 17:44:53 -0300

    Actualizacion deteccion y seguimiento
    Ahora el enemigo detecta cuando entramos en un rango y comienza el estado de perseguirnos, le agregue un extra para que el corte sea mas natural en la persecución.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,30 +1,58 @@
 using UnityEngine;
-using UnityEngine.AI; 
-
+using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
-    
-    public Transform target; 
+    public Transform target;
+    public float range;
+    public float extraRange;
 
-    
-    public float health = 100f;
+    public float updateInterval = 0.2f;
+    private float timer = 0f;
 
     private NavMeshAgent enemy;
+    private bool tracking = false;
+    private float distance;
 
-    void Start()
+    void Awake()
     {
-       
         enemy = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
+        if (target == null) return;
+
+        distance = Vector3.Distance(transform.position, target.position);
+
        
-        if (target != null)
+        if (distance <= range && !tracking)
         {
-            enemy.SetDestination(target.position);
+            tracking = true;
+        }
+        else if (distance > range + extraRange && tracking)
+        {
+            tracking = false;
+            enemy.ResetPath();
+        }
+
+        if (tracking)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= updateInterval)
+            {
+                enemy.SetDestination(target.position);
+                timer = 0f;
+            }
         }
     }
 
-   
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range + extraRange);
+    }
 }

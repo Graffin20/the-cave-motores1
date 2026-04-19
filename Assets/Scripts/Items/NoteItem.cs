336dacc7473417616e2b005202b11644f09991ec
Graffin20 <33572219+Graffin20@users.noreply.github.com>
2026-04-19 14:20:04 -0300

    Note system update

---
+++ Assets/Scripts/Items/NoteItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Note", menuName = "Inventory/Items/Note")]
+public class NoteItem : ItemDefinition
+{
+    [Header("Note Settings")]
+    [TextArea(5, 10)] // This makes text box in the Unity Inspector
+    public string noteContent = "Placeholder text here...";
+
+    public override bool Use(GameObject player)
+    {
+        if (NoteManager.Instance != null)
+        {
+            NoteManager.Instance.ShowNote(noteContent);
+            Debug.Log("Reading note: " + itemName);
+            return true; // Tells the inventory it was successfully used
+        }
+
+        Debug.LogWarning("Could not find a NoteManager in the scene!");
+        return false; // Tells the inventory it failed, so it won't be consumed
+    }
+}
